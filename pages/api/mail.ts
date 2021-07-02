2b6aeb6fd6c27436ba698eb8bef45c2b5fb8b116
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-07-02 14:31:57 -0300

    1.4
    Mailing system online

---
+++ pages/api/mail.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+const mail = require("@sendgrid/mail");
+
+mail.setApiKey(process.env.SENGRID_API_KEY);
+
+type Data = {
+	email: "";
+	subject: "";
+	message: "";
+};
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const body = JSON.parse(req.body);
+
+	const message = `
+    Email: ${body.email}\r\n
+    Subject: ${body.subject}\r\n
+    Message: ${body.message}\r\n
+  `;
+
+	const data = {
+		to: "tomas.solano.sanchez@gmail.com",
+		from: "tomas.solano.sanchez@icloud.com",
+		subject: "Portfolio form",
+		text: message,
+		html: message.replace(/\r\n/g, "<br>"),
+	};
+	try {
+		mail.send(data);
+		res.status(200).json({
+			email: body.email,
+			subject: body.subject,
+			message: body.message,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,51 @@
 import Head from "next/head";
+import { SyntheticEvent } from "react";
 import { useState } from "react";
 
+type formType = {
+	email: string;
+	subject: string;
+	message: string;
+};
+
 const Contact = () => {
-	const [emailForm, setEmailForm] = useState({
+	const [Ok, toggleOk] = useState(false);
+	const [emailForm, setEmailForm] = useState<formType>({
 		email: "",
 		subject: "",
 		message: "",
 	});
+
+	const inputSanitazion: () => boolean = () => {
+		return (
+			typeof emailForm.email === "string" &&
+			typeof emailForm.message === "string" &&
+			typeof emailForm.subject === "string" &&
+			emailForm.email.length < 40 &&
+			emailForm.message.length < 80 &&
+			emailForm.subject.length < 40 &&
+			emailForm.email.length > 0 &&
+			emailForm.message.length > 0 &&
+			emailForm.subject.length > 0
+		);
+	};
+
+	const handleSubmit = async (e: SyntheticEvent) => {
+		e.preventDefault();
+		const res = await fetch("/api/mail", {
+			method: "POST",
+			body: JSON.stringify(emailForm),
+		});
+		if (res.ok) {
+			console.log("succes");
+			setEmailForm({
+				email: "",
+				subject: "",
+				message: "",
+			});
+			toggleOk(true);
+		}
+	};
 
 	return (
 		<div>
@@ -30,6 +69,11 @@ const Contact = () => {
 						</p>
 					</div>
 					<div className='lg:w-1/2 md:w-2/3 mx-auto'>
+						<div
+							hidden={!Ok}
+							className='text-center text-lg rounded-lg bg-green-400 w-1/2 m-auto text-black mb-1 pb-2'>
+							Sent!
+						</div>
 						<div className='flex flex-wrap -m-2'>
 							<div className='p-2 w-1/2'>
 								<div className='relative'>
@@ -41,6 +85,13 @@ const Contact = () => {
 									<input
 										type='email'
 										id='email'
+										value={emailForm.email}
+										onChange={(e) =>
+											setEmailForm({
+												...emailForm,
+												email: e.target.value,
+											})
+										}
 										name='email'
 										className='w-full bg-gray-900 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
 									/>
@@ -56,6 +107,13 @@ const Contact = () => {
 									<input
 										type='text'
 										id='name'
+										value={emailForm.subject}
+										onChange={(e) =>
+											setEmailForm({
+												...emailForm,
+												subject: e.target.value,
+											})
+										}
 										name='name'
 										className='w-full bg-gray-900 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
 									/>
@@ -71,18 +129,29 @@ const Contact = () => {
 									<textarea
 										id='message'
 										name='message'
+										value={emailForm.message}
+										onChange={(e) =>
+											setEmailForm({
+												...emailForm,
+												message: e.target.value,
+											})
+										}
 										className='w-full bg-gray-900 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
-										defaultValue={""}
 									/>
 								</div>
 							</div>
 							<div className='p-2 w-full'>
-								<button className='flex mx-auto text-white bg-green-700 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
+								<button
+									disabled={!inputSanitazion()}
+									onClick={handleSubmit}
+									className='flex mx-auto text-white bg-green-700 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
 									Send
 								</button>
 							</div>
 							<div className='px-2 mb-3 w-full pt-8 mt-4 border-t border-gray-800 text-center'>
-								<a className='text-blue-500'>
+								<a
+									href='mailto: tomas.solano.sanchez@gmail.com'
+									className='text-blue-400 hover:underline'>
 									tomas.solano.sanchez@gmail.com
 								</a>
 							</div>
